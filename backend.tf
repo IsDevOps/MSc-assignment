85c7f7b8a645d2dbd7026959886eb2f697233f3c
IsDevOps <issyoseghale@gmail.com>
2025-04-03 11:25:57 +0100

    new update

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "medicate-dev-terraform-state"
-    key          = "dev/terraform.tfstate"
+    bucket       = "dev-terraform-state"
+    key          = "terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
+    use_lockfile = false
   }
 }
